1c74c1c1e62053369093b1be94d1db145e24f88a
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2012-01-15 23:22:33 +0200

    Replaced infblock.h with one from sources of actual zlib version 1.1.3.

--- neo/framework/zlib/infblock.h
+++ neo/framework/zlib/infblock.h
@@ -9,7 +9,7 @@
  */
 
 struct inflate_blocks_state;
-typedef struct inflate_blocks_state inflate_blocks_statef;
+typedef struct inflate_blocks_state FAR inflate_blocks_statef;
 
 extern inflate_blocks_statef * inflate_blocks_new OF((
     z_streamp z,
@@ -24,7 +24,7 @@ extern int inflate_blocks OF((
 extern void inflate_blocks_reset OF((
     inflate_blocks_statef *,
     z_streamp ,
-    uLong *));                  /* check value on output */
+    uLongf *));                  /* check value on output */
 
 extern int inflate_blocks_free OF((
     inflate_blocks_statef *,
@@ -32,7 +32,7 @@ extern int inflate_blocks_free OF((
 
 extern void inflate_set_dictionary OF((
     inflate_blocks_statef *s,
-    const Byte *d,  /* dictionary */
+    const Bytef *d,  /* dictionary */
     uInt  n));       /* dictionary length */
 
 extern int inflate_blocks_sync_point OF((
