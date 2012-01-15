b5053e4733fa89dc418f0debc53d7616fe44f55d
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2012-01-15 23:20:57 +0200

    Extracted infblock.h out of unzip.cpp.

--- neo/framework/Unzip.cpp
+++ neo/framework/Unzip.cpp
@@ -1489,45 +1489,7 @@ uLong crc32(uLong crc, const Byte *buf, uInt len)
     return crc ^ 0xffffffffL;
 }
 
-/* infblock.h -- header to use infblock.c
- * Copyright (C) 1995-1998 Mark Adler
- * For conditions of distribution and use, see copyright notice in zlib.h 
- */
-
-/* WARNING: this file should *not* be used by applications. It is
-   part of the implementation of the compression library and is
-   subject to change. Applications should only use zlib.h.
- */
-
-struct inflate_blocks_state;
-typedef struct inflate_blocks_state inflate_blocks_statef;
-
-extern inflate_blocks_statef * inflate_blocks_new OF((
-    z_streamp z,
-    check_func c,               /* check function */
-    uInt w));                   /* window size */
-
-extern int inflate_blocks OF((
-    inflate_blocks_statef *,
-    z_streamp ,
-    int));                      /* initial return code */
-
-extern void inflate_blocks_reset OF((
-    inflate_blocks_statef *,
-    z_streamp ,
-    uLong *));                  /* check value on output */
-
-extern int inflate_blocks_free OF((
-    inflate_blocks_statef *,
-    z_streamp));
-
-extern void inflate_set_dictionary OF((
-    inflate_blocks_statef *s,
-    const Byte *d,  /* dictionary */
-    uInt  n));       /* dictionary length */
-
-extern int inflate_blocks_sync_point OF((
-    inflate_blocks_statef *s));
+#include "zlib/infblock.h"
 
 /* simplify the use of the inflate_huft type with some defines */
 #define exop word.what.Exop
