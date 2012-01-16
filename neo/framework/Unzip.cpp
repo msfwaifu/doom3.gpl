5bc7863b1ca20cc42ce1ff929813f07d96d1561f
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2012-01-16 21:09:54 +0200

    Replaced zlib memory functions with standard library calls.

--- neo/framework/Unzip.cpp
+++ neo/framework/Unzip.cpp
@@ -1251,12 +1251,12 @@ extern "C"
 voidp zcalloc (voidp opaque, unsigned items, unsigned size)
 {
     if (opaque) items += size - size; /* make compiler happy */
-    return (voidp)Mem_ClearedAlloc(items*size);
+    return (voidp)calloc(items, size);
 }
 
 void  zcfree (voidp opaque, voidp ptr)
 {
-    Mem_Free(ptr);
+    free(ptr);
     if (opaque) return; /* make compiler happy */
 }
 
