7da9203d65df35c355e0722ddaf8fb54b9dd03c9
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2012-02-01 20:20:57 +0200

    Changed OSX to use system vorbis

--- neo/sound/snd_decoder.cpp
+++ neo/sound/snd_decoder.cpp
@@ -30,8 +30,8 @@ If you have questions concerning this license or the applicable additional terms
 #pragma hdrstop
 
 #include "snd_local.h"
-#include "OggVorbis/vorbis/codec.h"
-#include "OggVorbis/vorbis/vorbisfile.h"
+#include <vorbis/codec.h>
+#include <vorbis/vorbisfile.h>
 
 /*
 ===================================================================================
